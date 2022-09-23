6e0441909262ef64ed5bc52a0c87741aaec36770
Olexander753 <a862000@gmail.com>
2022-09-23 10:52:29 +0300

    create server and fix connect to postgres

---
+++ internal/server/server.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Olexander753/microservice-for-working-with-user-balance/internal/config"
+)
+
+type Server struct {
+	htppServer *http.Server
+}
+
+func (s *Server) Run(cfg config.Config, handler http.Handler) error {
+	log.Println("Run server")
+	s.htppServer = &http.Server{
+		Addr:           ":" + cfg.Server.Port,
+		Handler:        handler,
+		MaxHeaderBytes: 1 << 28,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+	return s.htppServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.htppServer.Shutdown(ctx)
+}
--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -3,21 +3,14 @@ package repository
 import (
 	"fmt"
 
+	"github.com/Olexander753/microservice-for-working-with-user-balance/internal/config"
 	"github.com/jmoiron/sqlx"
 )
 
-type Config struct {
-	Host     string
-	Port     string
-	Username string
-	Password string
-	DBName   string
-	SSLMode  string
-}
-
-func NewPostgresBD(cfg Config) (*sqlx.DB, error) {
+func NewPostgresBD(cfg config.Config) (*sqlx.DB, error) {
 	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
-		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
+		cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.Username,
+		cfg.Postgres.DBName, cfg.Postgres.Password, cfg.Postgres.SSLMode))
 
 	if err != nil {
 		return nil, err
